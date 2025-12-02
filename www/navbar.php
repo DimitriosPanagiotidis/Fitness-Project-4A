caca0af0847b46fabba4d93de2c03bc983033899
DimitriosPanagiotidis <dimitrispanagiotidis2003@gmail.com>
2025-12-02 10:53:17 +0100

    Add logout, navbar, register, register_process, and session_check files

---
+++ www/navbar.php
@@ -0,0 +1,8 @@
+<nav class="navigation-bar">
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="workouts.php">Workouts</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+</nav>
